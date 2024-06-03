c1f7993f6aea0e291bc1b776f18d6d37ee8b5149
lloydeu <yuuberuu@gmail.com>
2024-06-03 22:47:54 +0800

    disabled easter egg

--- scripts/script.js
+++
@@ -1,8 +0,0 @@
-function fireInTheHole() {
-    document.getElementByClassName(contact - info__avatar).src("assets/easy.png");
-}
-
-function stopFITH() {
-    document.getElementByClassName(contact - info__avatar).src("assets/avatar.png");
-}
-}
